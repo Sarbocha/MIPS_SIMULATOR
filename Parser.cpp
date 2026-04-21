3f52fba960fd8ae978a252ac72b807011770d763
Sarbocha <80576079+Sarbocha@users.noreply.github.com>
2026-04-21 13:20:21 -0500

    Update Parser.cpp

--- Parser.cpp
+++ Parser.cpp
@@ -3,6 +3,8 @@
 #include <sstream>
 #include <iostream>
 
+using namespace std;
+
 vector<Instruction> Parser::parseFile(string filename) {
     vector<Instruction> instructions;
     ifstream file(filename);
@@ -17,10 +19,61 @@ vector<Instruction> Parser::parseFile(string filename) {
         stringstream ss(line);
         ss >> inst.opcode;
 
-        // VERY BASIC parsing (you will improve this)
-        if (inst.opcode == "ADD") {
-            char comma;
+        char comma;
+
+        // ---------------- R-TYPE ----------------
+        if (inst.opcode == "ADD" || inst.opcode == "SUB" ||
+            inst.opcode == "MUL" || inst.opcode == "AND" ||
+            inst.opcode == "OR") {
+
+            // Format: OPCODE rd, rs, rt
             ss >> inst.rd >> comma >> inst.rs >> comma >> inst.rt;
+        }
+
+        // ---------------- ADDI ----------------
+        else if (inst.opcode == "ADDI") {
+            // Format: ADDI rt, rs, immediate
+            ss >> inst.rt >> comma >> inst.rs >> comma >> inst.immediate;
+        }
+
+        // ---------------- SHIFT ----------------
+        else if (inst.opcode == "SLL" || inst.opcode == "SRL") {
+            // Format: OPCODE rd, rt, shamt
+            ss >> inst.rd >> comma >> inst.rt >> comma >> inst.immediate;
+        }
+
+        // ---------------- LOAD ----------------
+        else if (inst.opcode == "LW") {
+            // Format: LW rt, offset, rs
+            ss >> inst.rt >> comma >> inst.immediate >> comma >> inst.rs;
+        }
+
+        // ---------------- STORE ----------------
+        else if (inst.opcode == "SW") {
+            // Format: SW rt, offset, rs
+            ss >> inst.rt >> comma >> inst.immediate >> comma >> inst.rs;
+        }
+
+        // ---------------- BRANCH ----------------
+        else if (inst.opcode == "BEQ") {
+            // Format: BEQ rs, rt, offset
+            ss >> inst.rs >> comma >> inst.rt >> comma >> inst.immediate;
+        }
+
+        // ---------------- JUMP ----------------
+        else if (inst.opcode == "J") {
+            // Format: J address
+            ss >> inst.address;
+        }
+
+        // ---------------- NOP ----------------
+        else if (inst.opcode == "NOP") {
+            // do nothing
+        }
+
+        // ---------------- UNKNOWN ----------------
+        else {
+            cout << "Warning: Unknown opcode -> " << inst.opcode << endl;
         }
 
         instructions.push_back(inst);
